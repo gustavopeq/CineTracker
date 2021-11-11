1974a554b26600461f00ef26495c891a33ddad4c
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2021-11-10 19:56:08 -0800

    Drawer added with Lottie icon animation

--- app/src/main/java/gustavo/projects/restapi/NavGraphActivity.kt
+++ app/src/main/java/gustavo/projects/restapi/NavGraphActivity.kt
@@ -2,11 +2,14 @@ package gustavo.projects.restapi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class NavGraphActivity: AppCompatActivity() {
 
@@ -19,10 +22,16 @@ class NavGraphActivity: AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
 
         // Action bar to create back arrow and page title
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.popularMoviesFragment),
+            drawerLayout = drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
     }
 
     //Back arrow to return stack
